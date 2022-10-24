5b1ae3638f33509c827d60c64a9ef411a1564ef0
Ee0Rk <erikottofrederik@gmail.com>
2022-10-24 21:00:25 +0200

    created projects
    created custom sln file in root
    also edited names of the forms

---
+++ rascal_controller/window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rascal_controller
+{
+    public partial class window : Form
+    {
+        public window()
+        {
+            InitializeComponent();
+        }
+    }
+}
